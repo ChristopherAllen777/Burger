86318099b4ea5f09b2703738d17a204a4d8c6403
Chris Allen <christopherallen777@gmail.com>
2017-07-27 18:33:09 -0500

    eat burger

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ("XXL Burger", false, '20170612');
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ("Chicken Burger", false, '20170612');
+
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES ("Vegan Burger", false, '20170612');
+
+SELECT * FROM burgers_db.burgers;
